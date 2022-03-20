a52a2484ef4f69a33dcc21a7e484631fb8b5c3ca
judyblue8 <74081546+judyblue8@users.noreply.github.com>
2022-03-20 18:18:26 -0400

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -11,25 +11,25 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     content = request.get_json(silent=True)
-    sig = content.get("sig")
-    pload = content.get("payload")
-    pk=pload.get("pk")
-    platform=pload.get("platform")
-    msg=json.dumps(pload)
-    result = False
-    if platform == "Ethereum":
-        eth_encoded_msg =eth_account.messages.encode_defunct(text = msg)
-        if eth_account.Account.recover_message(eth_encoded_msg ,sig = signiture) == pk:
-            print("Ethereum signiture is verified!")
-            result = True
+    signature=content.get("sig")
+    payload=content.get("payload")
+    pk=payload.get("pk")
+    platform=payload.get("platform")
+    msg=json.dumps(payload)
+    result=False
+    if platform=="Ethereum":
+        eth_encoded_msg =eth_account.messages.encode_defunct(text=msg)
+        if eth_account.Account.recover_message(eth_encoded_msg ,signature=signature)==pk:
+            print("Eth sig verifies!")
+            result=True
     elif platform =="Algorand":
         #algo_sk = 'VDw/rBQ6ETI8kkpsXa3KQ7q3FFVKdNgL9Oem59c2Nixe4LyxB6otPKwHKpcWcJ2QxrBjPVj1XgON58ssS7I/JA=='
         #algo_pk = 'L3QLZMIHVIWTZLAHFKLRM4E5SDDLAYZ5LD2V4A4N47FSYS5SH4SAFAIYVQ'
-        #algo_sig_str = algosdk.util.sign_bytes(pload.encode('utf-8'), algo_sk)
-        if algosdk.util.verify_bytes(msg.encode('utf-8'), sig, pk):
-            print("Algorand signiture is verified!")
-            result = True
-
+        #algo_sig_str = algosdk.util.sign_bytes(payload.encode('utf-8'), algo_sk)
+        if algosdk.util.verify_bytes(msg.encode('utf-8'), signature, pk):
+            print("Algo sig verifies!")
+            result=True
+            
     #Check if signature is valid
     result = True #Should only be true if signature validates
     return jsonify(result)
