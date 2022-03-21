09997dbe48a29976066c2f353e74a59f8a72077c
judyblue8 <74081546+judyblue8@users.noreply.github.com>
2022-03-20 22:20:58 -0400

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -12,21 +12,21 @@ app.url_map.strict_slashes = False
 def verify():
     content = request.get_json(silent=True)
     signature=content.get("sig")
-    payload=content.get("payload")
-    pk=payload.get("pk")
-    platform=payload.get("platform")
-    msg=json.dumps(payload)
+    pload=content.get("payload")
+    pk=pload.get("pk")
+    platform=pload.get("platform")
+    message=json.dumps(pload)
     result=False
     if platform=="Ethereum":
-        eth_encoded_msg =eth_account.messages.encode_defunct(text=msg)
+        eth_encoded_msg =eth_account.messages.encode_defunct(text=message)
         if eth_account.Account.recover_message(eth_encoded_msg ,signature=signature)==pk:
             print("Eth sig verifies!")
             result=True
     elif platform =="Algorand":
         #algo_sk = 'VDw/rBQ6ETI8kkpsXa3KQ7q3FFVKdNgL9Oem59c2Nixe4LyxB6otPKwHKpcWcJ2QxrBjPVj1XgON58ssS7I/JA=='
         #algo_pk = 'L3QLZMIHVIWTZLAHFKLRM4E5SDDLAYZ5LD2V4A4N47FSYS5SH4SAFAIYVQ'
-        #algo_sig_str = algosdk.util.sign_bytes(payload.encode('utf-8'), algo_sk)
-        if algosdk.util.verify_bytes(msg.encode('utf-8'), signature, pk):
+        #algo_sig_str = algosdk.util.sign_bytes(pload.encode('utf-8'), algo_sk)
+        if algosdk.util.verify_bytes(message.encode('utf-8'), signature, pk):
             print("Algo sig verifies!")
             result=True
             
